dad3856e3650da0e832ede6e6def7a667b20864a
Sean Joseph <69924413+sejogit28@users.noreply.github.com>
2021-09-06 10:09:53 -0500

    Reverted to gifs on project cards

--- src/components/ProjectCardListData.js
+++ src/components/ProjectCardListData.js
@@ -3,7 +3,7 @@ const projectCardListData = [
     {
         id: 1,
         title:"Bug Track",
-        imageSrc:"/Photos/BlazorLogo.png",
+        imageSrc:"/Photos/BugTrackerGif.gif",
         description: "A bug Tracker built using .NET 5(MVC), Entity Framework, Bootstrap and Microsoft SQL Server",
         liveLink:"https://the-bug-track.azurewebsites.net/",
         gitHubLink: "https://github.com/sejogit28/BugTrackerTime2Sol"
@@ -12,7 +12,7 @@ const projectCardListData = [
     {
         id: 2 ,
         title:"Expense Track",
-        imageSrc:"/Photos/BlazorLogo.png",
+        imageSrc:"/Photos/ExpenseTrackerGif.gif",
         description: "An expense tracker built with Blazor Web Assembly, .NET 5 Web Api, and Microsoft SQL Server",
         liveLink:"https://exptrackerwasmclient20210823222931.azurewebsites.net",
         gitHubLink: "https://github.com/sejogit28/ExpenseTrackerSol"
@@ -20,7 +20,7 @@ const projectCardListData = [
     {
         id: 3,
         title:"Coding Mind",
-        imageSrc:"/Photos/BlazorLogo.png",
+        imageSrc:"/Photos/MernBlogGif.gif",
         description: "A personal blog application built using the MERN(MongoDb, Express, React and Node).",
         liveLink:"https://fervent-darwin-5bcd5e.netlify.app/",
         gitHubLink: "https://github.com/sejogit28/MERN-Blog"
