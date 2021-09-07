1be1b1a17a0a8e5ca08645c7cac2233884a9a699
Sean Joseph <69924413+sejogit28@users.noreply.github.com>
2021-09-07 09:59:29 -0500

    Fixed broken link

--- src/components/ProjectCardListData.js
+++ src/components/ProjectCardListData.js
@@ -2,31 +2,31 @@
 const projectCardListData = [
     {
         id: 1,
-        title:"Bug Track",
-        imageSrc:"/Photos/BugTrackCaptureCropped.mp4",
+        title: "Bug Track",
+        imageSrc: "/Photos/BugTrackCaptureCropped.mp4",
         description: "A bug tracker built using .NET 5(MVC), Entity Framework, Bootstrap and Microsoft SQL Server",
-        liveLink:"https://the-bug-track.azurewebsites.net/",
+        liveLink: "https://thebugtrack.azurewebsites.net/",
         gitHubLink: "https://github.com/sejogit28/BugTrackerTime2Sol"
-        
+
     },
     {
-        id: 2 ,
-        title:"Expense Track",
-        imageSrc:"/Photos/ExpenseTrackCaptureCropped.mp4",
+        id: 2,
+        title: "Expense Track",
+        imageSrc: "/Photos/ExpenseTrackCaptureCropped.mp4",
         description: "An expense tracker built with Blazor Web Assembly, .NET 5 Web Api, and Microsoft SQL Server",
-        liveLink:"https://exptrackerwasmclient20210823222931.azurewebsites.net",
+        liveLink: "https://exptrackerwasmclient20210823222931.azurewebsites.net",
         gitHubLink: "https://github.com/sejogit28/ExpenseTrackerSol"
     },
     {
         id: 3,
-        title:"Coding Mind",
-        imageSrc:"/Photos/CodingMindCaptureCropped.mp4",
+        title: "Coding Mind",
+        imageSrc: "/Photos/CodingMindCaptureCropped.mp4",
         description: "A personal blog application built using the MERN(MongoDb, Express, React and Node).",
-        liveLink:"https://fervent-darwin-5bcd5e.netlify.app/",
+        liveLink: "https://fervent-darwin-5bcd5e.netlify.app/",
         gitHubLink: "https://github.com/sejogit28/MERN-Blog"
     },
-    
-    
+
+
 ]
 
 export default projectCardListData;
